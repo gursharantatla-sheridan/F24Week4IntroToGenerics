5475d931ccc80684028e4685e2cbe61da5d05052
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-23 08:20:05 -0400

    Add project files.

---
+++ F24Week4IntroToGenerics/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace F24Week4IntroToGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // boxing
+            int i = 123;
+            object obj = i;
+
+
+            // unboxing
+            if (obj is int)
+                i = (int)obj;
+        }
+
+        static bool AreEqual(object a, object b)
+        {
+            return a.Equals(b);
+        }
+    }
+}
