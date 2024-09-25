683442a414fa3f4f070e16fbcb241cb45a4e6063
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-25 16:23:26 -0400

    dictionary example

---
+++ DictionaryExample/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExample
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id,5} {Name,-10} {Salary,10:C}";
+        }
+    }
+}
